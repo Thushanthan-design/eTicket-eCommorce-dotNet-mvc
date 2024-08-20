f5fa67449475f952330bd7c43cefcb2acaab2785
Thushanthan <mohanthushanthan2@gmail.com>
2024-08-20 22:41:49 +0530

    Add project files.

---
+++ eTicket-eCommorce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTicket_eCommorce.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
